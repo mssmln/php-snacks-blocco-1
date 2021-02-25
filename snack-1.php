a76397d069b421ba4e4a7ee0abe2dfaddf54ac65
Massimiliano <m.croci.ly@gmail.com>
2021-02-25 14:30:09 +0100

    working on snack 2

--- snack-1.php
+++ snack-1.php
@@ -32,9 +32,25 @@ Olimpia Milano - Cantù | 55 - 60 -->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
   <title>snack 1 squadre di basket</title>
 </head>
 <body>
+    <?php # il count corrisponde al metodo length di js
+    for ($i = 0; $i < count($matches); $i++){
+      // var_dump($matches[$i]); # mostra il contenuto di matches per ogni indice
+      $casa = $matches[$i]["casa"]; // li salviamo nelle variabili
+      $ospite = $matches[$i]["ospite"];
+      $score = $matches[$i]["score"];
+    ?>
+    <div class="match">
+        <h2><?php echo $casa ." - " // li stampiamo ?></h2>
+        <h2> <?php echo " - " .$ospite ." | "?></h2>
+        <h2><?php echo "  " .$score[0] ." - " .$score[1]?></h2>
+    </div>
+
+
+    <?php } # chiusura del ciclo for?>
 
 </body>
 </html>
