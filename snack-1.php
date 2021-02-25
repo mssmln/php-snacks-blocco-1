d5b3d78f7585cef59d8d9360b4c0b166cb3acf13
Massimiliano <m.croci.ly@gmail.com>
2021-02-25 13:49:37 +0100

    init

---
+++ snack-1.php
@@ -0,0 +1,40 @@
+<!-- PHP Snack 1:
+Creiamo un array 'matches' contenente altri array i quali rappresentano delle partite di basket di un’ipotetica tappa del calendario. Ogni array della partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55 - 60 -->
+
+<?php
+  $matches = [
+    [
+      'casa' => 'Vigevano',
+      'ospite' => 'Milano',
+      'score' => [30,50]
+    ],
+    [
+      'casa' => 'Vicenza',
+      'ospite' => 'Roma',
+      'score' => [50,100]
+    ],
+    [
+      'casa' => 'Venezia',
+      'ospite' => 'Firenze',
+      'score' => [70,110]
+    ],
+  ]
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>snack 1 squadre di basket</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ snack-2.php
@@ -0,0 +1,6 @@
+<!-- PHP Snack 2:
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che:
+1. name sia più lungo di 3 caratteri,
+2. mail contenga un punto e una chiocciola
+3. age sia un numero.
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”. -->
